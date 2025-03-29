e87c43e381fed6b609ebbe9d9eb9ee53cb1a4ea1
Alex-Medeir0s <alexmedeiros157@outlook.com>
2025-03-29 10:18:52 -0300

    adiciona componente Result e lógica de conversão de moeda no Form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -7,7 +7,38 @@ import {getCotacao} from "../../../services/api";
 
 
 export default function Form() {
+    //secao da logica
+    const [real, setReal] = useState(null);
+    const [resultado, setResultado] = useState(null);
+    const [msg, setMsg] = useState("");
+    const [cotacao, setCotacao] = useState(null);
 
+    //funcao que e chamada pelo botao da tela
+    function validar() {
+
+        if(real!=null){
+            converter()
+            setReal(null)
+        }else{
+            setMsg("Informe um valor para conversão")
+            setResultado(null)
+        }
+
+    }
+
+    //funcao que faz a chamada da API e converte o valor
+    async function converter() {
+        const aux = await getCotacao()
+
+        setCotacao(aux[0])
+        setMsg(aux[1])
+
+        return setResultado((real/cotacao).toFixed(2))
+
+    }
+
+
+//secao da montagem da tela
     return(
 
         <View style={styles.formContext}>
