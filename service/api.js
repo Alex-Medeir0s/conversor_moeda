4a606005fca5016bbd31efef5cfe7f314c905f0f
Alex-Medeir0s <alexmedeiros157@outlook.com>
2025-03-29 09:27:23 -0300

    adiciona tratamento da resposta da API para obter valores de cotação do dólar

--- service/api.js
+++ service/api.js
@@ -8,8 +8,16 @@ export default async function getCotacao(){
     await axios.get(url)
     .then(function (response) {
 
-        //recupera toda a resposta da chamada da API 
-        const data = response.data;
+        //recupera toda a resposta da chamada da API, metodo GET
+        const data = response.data
+
+        const ask = data.USDBRL.ask
+        const name = data.USDBRL.name
+
+        results = [ask, name]
+        console.log("Cotacao do dia")
+        console.log(ask)
+        console.log(name)
 
     })
     .catch(function (error) {
